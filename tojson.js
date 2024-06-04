50139b8382b99726afa63bd18782c424397efdee
송이현 <songehyun00@gmail.com>
2024-06-04 16:58:44 +0900

    해결 완료

--- tojson.js
+++ tojson.js
@@ -4,4 +4,24 @@ let datacsv = fs.readFileSync("./Data.csv");
 
 let datacsvtostring = datacsv.toString();
 
-console.log(datacsvtostring);
+// function csvtojson(json){}
+
+let datastringsplit = datacsvtostring.split("\r\n");
+
+const dataarray = [];
+
+let datatitle = datastringsplit[0].split(",");
+
+for (let i = 1; i < datastringsplit.length; i++) {
+  let obj = {};
+
+  let datavalue = datastringsplit[i].split(",");
+
+  for (let j = 0; j < datatitle.length; j++) {
+    obj[datatitle[j]] = datavalue[j];
+  }
+
+  dataarray.push(obj);
+}
+
+console.log(dataarray);
